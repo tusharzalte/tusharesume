865a08b138a3bc95b292c7e5f7367c1343ec49ee
Rajnish <b421037@iiit-bh.ac.in>
2023-06-02 17:51:19 +0530

    Warnings, errors and orientation fixed

--- client1/src/Pages/Login.js
+++ client1/src/Pages/Login.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Form, Input, Button,  message, Spin } from "antd";
+import { Form, Input, Button, message, Spin } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import "../Resources/Stylesheets/authentication.css";
 import Footer from "../Components/Footer";
 import axios from "axios";
 
-function Register() {
+export default function Login() {
   const [loading, setLoading] = useState(false);
   const [captchaValue, setCaptchaValue] = useState("");
   const [captchaInput, setCaptchaInput] = useState("");
@@ -13,7 +13,10 @@ function Register() {
   const onFinish = async (values) => {
     setLoading(true);
     try {
-      const user = await axios.post("api/user/login", { ...values, captchaInput });
+      const user = await axios.post("api/user/login", {
+        ...values,
+        captchaInput,
+      });
       message.success("Login successfull");
       localStorage.setItem("tusharresume-users", JSON.stringify(user.data));
       setLoading(false);
@@ -23,12 +26,11 @@ function Register() {
       message.error("Login failed");
     }
   };
-  const captchahandler = async()=>{
+  const captchahandler = async () => {
     const response = await axios.get("/api/user/captcha");
     setCaptchaValue(response.data);
     setCaptchaInput("");
-
-  }
+  };
   useEffect(() => {
     const fetchCaptcha = async () => {
       try {
@@ -40,10 +42,10 @@ function Register() {
         console.error("Failed to fetch CAPTCHA", error);
       }
     };
-  
+
     fetchCaptcha();
   }, []);
-  
+
   useEffect(() => {
     if (localStorage.getItem("tusharresume-users")) {
       navigate("/home");
@@ -55,7 +57,11 @@ function Register() {
       <div className="authenticationParent">
         {loading && <Spin size="large" />}
         <h1 className="brand">Resume Builder</h1>
-        <Form layout="vertical" onFinish={onFinish}>
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          style={{ marginTop: "100px" }}
+        >
           <h1>Login</h1>
           <hr />
           <Form.Item name="username" label="Username">
@@ -65,50 +71,61 @@ function Register() {
             <Input type="password" />
           </Form.Item>
           <Form.Item label="CAPTCHA(Enter the below code to verify)">
-          <div className="captcha-container">
-  <span
-    className="captcha-value"
-    style={{
-      display: "inline-block",
-      padding: "6px 10px",
-      fontSize: "18px",
-      fontWeight: "bold",
-      backgroundColor: "#f5f5f5",
-      border: "1px solid #ccc",
-      borderRadius: "4px",
-      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-      marginRight: "10px",
-    }}
-  >
-    {captchaValue}
-  </span>
+            <div className="captcha-container">
+              <span
+                className="captcha-value"
+                style={{
+                  display: "inline-block",
+                  padding: "6px 10px",
+                  fontSize: "18px",
+                  fontWeight: "bold",
+                  backgroundColor: "#f5f5f5",
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                  marginRight: "10px",
+                }}
+              >
+                {captchaValue}
+              </span>
 
-  <Input
-    className="captcha-input"
-    value={captchaInput}
-    onChange={(e) => setCaptchaInput(e.target.value)}
-    style={{
-      width: "100px",
-      height: "40px",
-      border: "1px solid #ccc",
-      borderRadius: "4px",
-      padding: "6px 10px",
-      fontSize: "16px",
-      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-    }}
-  />
-  <Button type="link" onClick={captchahandler} style={{ marginLeft: "10px" }}>
-      Refresh
-    </Button>
-</div>
-
-        </Form.Item>
+              <Input
+                className="captcha-input"
+                value={captchaInput}
+                onChange={(e) => setCaptchaInput(e.target.value)}
+                style={{
+                  width: "100px",
+                  height: "40px",
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  padding: "6px 10px",
+                  fontSize: "16px",
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                }}
+              />
+              <Button
+                type="link"
+                onClick={captchahandler}
+                style={{ marginLeft: "10px" }}
+              >
+                Refresh
+              </Button>
+            </div>
+          </Form.Item>
 
           <div className="d-flex align-items-center justify-content-between mt-4">
-            <Button className="button custom-btn btn-1" type="primary" htmlType="submit">
+            <Button
+              className="button custom-btn btn-1"
+              type="primary"
+              htmlType="submit"
+            >
               <Link to="/register">Register</Link>
             </Button>
-            <Button className="button custom-btn btn-1" type="primary" htmlType="submit">
+            <Button
+              className="button custom-btn btn-1"
+              type="primary"
+              htmlType="submit"
+            >
               Login
             </Button>
           </div>
@@ -118,5 +135,3 @@ function Register() {
     </>
   );
 }
-
-export default Register;
